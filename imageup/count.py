a7e1203ae0b275e5f06f9335f0caf3ddd95960ed
layman <zhufeng3612@163.com>
2015-01-03 22:50:36 +0800

    time

--- imageup/count.py
+++ imageup/count.py
@@ -1,18 +1,25 @@
 __author__ = 'zhufeng'
 
-import os;
+import os,datetime;
 from sys import argv;
 
-def getFile(path):
-    print("dddd")
 
 
 try:
     script,path = argv;
 except:
-    print("with path please")
+    print("with path name please--------------")
 
 print "listing path:%s"%path
-getFile(path)
+count = 0;
+for path,dirs,files in os.walk(path):
+    # print(path,dirs,files)
+    for file in files:
+        count+=1
+        timestamp = os.path.getctime(os.path.join(path, file));
+        date = datetime.datetime.fromtimestamp(timestamp)
+        print(file,date.strftime('%Y-%m-%d %H:%M:%S'))
+print(count)
+
 
 
